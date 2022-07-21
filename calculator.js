65945218f8ab0ae23ba0af5e1670b56ca37f2fd1
Krishna <libraryismyparadise@gmail.com>
2022-07-21 15:28:56 +0545

    Add test for Calcualtor

---
+++ calculator.js
@@ -0,0 +1,19 @@
+class Calculator {
+  static addition = (a, b) => {
+    return a + b;
+  };
+
+  static substraction = (a, b) => {
+    return a - b;
+  };
+
+  static multiplication = (a, b) => {
+    return a * b;
+  };
+
+  static division = (a, b) => {
+    return a / b;
+  };
+}
+
+module.exports = Calculator;
