39718ae4d2b416110db17c554c1bddf379ade71e
Krishna <libraryismyparadise@gmail.com>
2022-07-21 15:19:35 +0545

    Add testing for reverseString()

---
+++ reverseString.test.js
@@ -0,0 +1,5 @@
+const reverseString = require("./reverseString");
+
+test("If string is reversed", () => {
+  expect(reverseString("Jumbo")).toBe("obmuJ");
+});
--- stringLength.test.js
+++ stringLength.test.js
@@ -1,11 +1,12 @@
 const stringLength = require("./stringLength");
 
-test("check the length of the string", () => {
-  expect(stringLength("Hello")).toBe(5);
+
+test("If string length is less than 0", () => {
+  expect(stringLength("")).toBe("Testing Error");
 });
 
-test("If string legth is less than 0", () => {
-  expect(stringLength("")).toBe("Testing Error");
+test("check the length of the string", () => {
+  expect(stringLength("Hello")).toBe(5);
 });
 
 test("If string length is more than 10", () => {
